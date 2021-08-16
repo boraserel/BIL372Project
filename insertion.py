87d6ca4f154235814a7a082d580f267f2e168610
bsumluludan <60651647+bsumluludan@users.noreply.github.com>
2021-08-16 17:56:04 +0300

    insertion.py added

---
+++ insertion.py
@@ -0,0 +1,125 @@
+import pandas as pd
+from sqlalchemy.exc import IntegrityError
+
+from model import db
+
+from model import Employee,Customer,CustomerLogin,Carrier,Chemist,Company,Content,Confirms,ConsistOf,Customer_Service,EmployeesOfCompany,Accountant,Includes,EmployeeLogin,AdminLogin,Admin,Analyst,Takes,Order,Sells,Department,Manages,Material,MemberLicenceType,OrderDate,Packages,Perfume,ProductFeature,Supplier,Vehicle,VehicleFeatures,Worker,ProductQuantity
+import pandas as pd
+
+data = db.get_engine()  # db is the one from the question
+file_csv = 'Cart.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Cart",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Course.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Course",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Customer.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Customer",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Enrolls.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Enrolls",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Instructor.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Instructor",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Needed.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Needed",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Order.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Order",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Product.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Product",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action
+
+file_csv = 'Purchased.csv'
+
+# Read CSV with Pandas
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    dcsv = pd.read_csv(file)
+
+# Insert to DB
+for i in range(len(dcsv)):
+    try:
+        dcsv.iloc[i:i+1].to_sql(name="Purchased",if_exists='append',con = data,index=False)
+    except IntegrityError:
+        pass #or any other action

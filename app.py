00981bbf80147d610c0e5dba8d245c381696fbf6
bsumluludan <60651647+bsumluludan@users.noreply.github.com>
2021-08-17 15:46:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, flash, url_for, redirect, render_template, make_response
-from model import db, app, instructorlogin, needed
-from model import course,instructor,enrolls,needed
+from sqlalchemy.orm import query
+from model import db, app, instructorlogin, needed, product
+from model import course,instructor,enrolls,needed,customerlogin,customer
 from flask_sqlalchemy import SQLAlchemy
 
 @app.route("/")
@@ -40,7 +41,6 @@ def instructor_page_checkin():
                 return response
 
             else:
-                flash('Incorrect Email or Password')
                 return render_template('instructor_login.html')
 
     #if username and password are not in database redirect instructor page
@@ -65,19 +65,20 @@ def instructor_page():
         enrolls.query.filter_by(enrolls_course_id = value).delete()
         db.session.delete(course_toBeDeleted)
         db.session.commit()
-        
         inst = instructor.query.filter_by(inst_id=instid).first()
         courses = course.query.filter_by(course_inst_id=instid).all()
         response = make_response(render_template('instructor_page.html',instructor_info=inst,courses_by_instructor=courses))
     if id == 'editbutton':
         inst = instructor.query.filter_by(inst_id=instid).first()
         course_toBeEdited = course.query.filter_by(course_id = value).first()
+        print(course_toBeEdited.course_name)
         response = make_response(render_template('instructor_edit.html',instructor_info=inst,selected_course=course_toBeEdited))  
         response.set_cookie("course_id", str(course_toBeEdited.course_id))      
     
     
 
     return response
+
 
 @app.route('/instructor_edit', methods=['GET', 'POST'])
 def instructor_edit():
@@ -86,8 +87,6 @@ def instructor_edit():
     inst = instructor.query.filter_by(inst_id=instid).first()   
     course_toBeEdited = course.query.filter_by(course_id = coursesid).first()
     if request.method == 'POST':
-             
-
              course_toBeEdited.course_name= request.form.get('Name')
              course_toBeEdited.course_category = request.form.get('Category')
              course_toBeEdited.course_level = request.form.get('Level')
@@ -100,19 +99,65 @@ def instructor_edit():
              response = make_response(render_template('instructor_page.html',instructor_info=inst,courses_by_instructor=courses))
              return response
 
-@app.route('/customer_page', methods=['GET', 'POST'])
-def customer_page():
+@app.route('/customer_page_checkin', methods=['GET', 'POST'])
+def customer_page_checkin():
     if request.method == 'POST':
         if not request.form['username'] or not request.form['password']:
-            flash('Please enter all the fields', 'error')
+            return render_template('instructor_login.html')
         else:
             username = request.form.get('username')
             password = request.form.get('password')
+        custlog = customerlogin.query.filter_by(customerlogin_user = username, customerlogin_pass = password).first()
+        if custlog:  # if a user is found, we want to redirect back to signup page so user can try again
+            cust = customer.query.filter_by(cust_id=custlog.customerlogin_id).first()
+            response = make_response(render_template('customer_page.html',customer_info=cust))
+            response.set_cookie("cust_id", str(cust.cust_id))
+            response.set_cookie("custlog_id", str(custlog.customerlogin_id))
+            return response
 
-    #if username and password are not in database redirect instructor page
-    #return render_template('instructor_login.html')
-    #else redirect instructor page
-    return render_template('customer_page.html')
+        else:
+            return render_template('instructor_login.html')
+
+
+@app.route('/customer_page', methods=['GET', 'POST'])
+def customer_page():
+    custlogid = request.cookies.get('custlog_id',type=int)
+    customer_info =customer.query.filter_by(cust_id = custlogid).first()
+    return render_template('customer_page.html',customer_info=customer_info)
+
+@app.route('/all_courses', methods=['GET', 'POST'])
+def all_courses():
+
+    if request.method == 'POST':
+        searched_course = request.form.get('search_bar')  # course id cannot change
+        print(searched_course)
+        if(searched_course == ''):
+            filtered_courses = course.query.all()
+        else:
+            searched_course_tag = "%{}%".format(searched_course)
+            filtered_courses = course.query.filter(course.course_category.ilike(searched_course_tag))
+        return render_template('all_courses.html', all_courses=filtered_courses)
+
+    all_courses = course.query.all()
+
+    id = request.args.get('id') #==show_related ise
+    value = request.args.get('value') #
+    if id=='show_related':
+
+        selected_course = course.query.filter_by(course_id = value).first()
+        need = needed.query.filter_by(needed_course_id = value).all()
+        list = []
+        for x in need:
+            list.append(product.query.filter_by(prod_id = x.needed_prod_id).first())
+        return render_template('all_courses_related_product.html',selected_course=selected_course,related_products=list)
+
+    return render_template('all_courses.html',all_courses=all_courses)
+
+@app.route('/all_courses_related_product', methods=['GET', 'POST'])
+def all_courses_related_product():
+    return render_template('all_courses_related_product.html')
+
+
 
 @app.route('/admin_page', methods=['GET', 'POST'])
 def admin_page():
